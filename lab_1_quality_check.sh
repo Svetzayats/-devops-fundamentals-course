3b9b54eec9b6486922aec6b5fbe11f2b583f043d
svetzayats <svetzayats@gmail.com>
2023-03-20 19:26:59 +0400

    Fix review comments + add result of pipleline script

--- lab_1_quality_check.sh
+++ lab_1_quality_check.sh
@@ -4,4 +4,12 @@ project_folder=~/Projects/shop-angular-cloudfront
 
 # e2e falls with Cannot find module 'jasmine-spec-reporter'
 # npm test runs headless chrome and nothing happens - 0 from 0 success 
-cd $project_folder && npm run lint && npm run e2e && npm audit
+cd $project_folder 
+echo "Launching linting..."
+npm run lint 
+
+echo "Launching e2e..."
+npm run e2e 
+
+echo "Launching npm audit..."
+npm audit
